833c180fd4c57d4db00985a7a0b2b8af249e623c
Adam Alder <atariq11700@gmail.com>
2019-11-28 23:22:13 -0700

    hi

--- blackjack.h
+++ blackjack.h
@@ -3,4 +3,14 @@
 
 class Blackjack{
     public:
+        int players;
+        bool dealer;
+    Blackjack(int x,bool y){
+        players = x;
+        dealer = y;
+        newGame(players,dealer);
+    };
+    void newGame(int num_of_players, bool is_there_dealer){
+        
+    }
 };
